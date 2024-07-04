e566c9a076528daab0952d4d1597d2b28456b3d8
SinmisolaE <sinmisolaajao@gmail.com>
2024-07-04 22:29:18 +0000

    3-to_str.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""
+ that takes a float a and a float b as arguments
+"""
+def add(a: float, b: float) -> float:
+    """ returns their sum as a float"""
+    return(a + b)
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+function floor which takes a float n as argument
+"""
+
+
+def floor(n: float) -> int:
+    """ returns the floor of the float."""
+    return int(n)
